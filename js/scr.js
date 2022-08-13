2b67fe740d1af3cd8d51d667304cc7383e4250e7
Gustavo Campos <98385777+gustawebp@users.noreply.github.com>
2022-08-13 02:20:47 -0300

    Add files via upload

---
+++ js/scr.js
@@ -0,0 +1,22 @@
+
+function prec() {
+
+document.getElementById('img-prec').style.display = "block"
+
+}
+
+
+
+
+function buy() {
+
+    document.getElementById('setor').style.display = "block"
+
+}
+
+function seguir() {
+    window.location.href = "buy.html"
+
+
+}
+
